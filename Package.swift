b6c8a03c800ede1b0f7855da845bd0a192d7013a
B.T. Franklin <brandon.franklin@gmail.com>
2019-08-30 21:33:01 -0700

    Added a unit test for Top200PersonGivenName, and bumped the Dunesailer Utilities dependency version up to the latest (0.4.0).

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["Wordsmith"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/dunesailer/Utilities.git", from: "0.2.0"),
+        .package(url: "https://github.com/dunesailer/Utilities.git", from: "0.4.0"),
     ],
     targets: [
         .target(
---
+++ Tests/WordsmithTests/Top200PersonGivenNameTests.swift
@@ -0,0 +1,59 @@
+//  Created by B.T. Franklin on 8/30/19
+
+import XCTest
+@testable import Wordsmith
+
+class Top200PersonGivenNameTests: XCTestCase {
+    
+    func testInitializer() {
+                
+        for _ in 1...100 {
+            let name = Top200PersonGivenName(gender: .male)
+            XCTAssertNotNil(name.description)
+            print("Male: \(name)")
+        }
+        
+        for _ in 1...100 {
+            let name = Top200PersonGivenName(gender: .female)
+            XCTAssertNotNil(name.description)
+            print("Female: \(name)")
+        }
+    }
+    
+    func testUniqueness() {
+        var generatedValues = Set<Top200PersonGivenName>()
+        var consecutiveRetries = 0
+        
+        while consecutiveRetries < 1000 {
+            let value = Top200PersonGivenName(gender: .male)
+            
+            if generatedValues.contains(value) {
+                consecutiveRetries += 1
+            } else {
+                generatedValues.insert(value)
+                consecutiveRetries = 0
+            }
+        }
+        
+        print("Could no longer generate unique male names after \(consecutiveRetries) consecutive retries.")
+        print("Final count of unique values: \(generatedValues.count)")
+        
+        generatedValues.removeAll()
+        consecutiveRetries = 0
+        
+        while consecutiveRetries < 1000 {
+            let value = Top200PersonGivenName(gender: .female)
+            
+            if generatedValues.contains(value) {
+                consecutiveRetries += 1
+            } else {
+                generatedValues.insert(value)
+                consecutiveRetries = 0
+            }
+        }
+        
+        print("Could no longer generate unique female names after \(consecutiveRetries) consecutive retries.")
+        print("Final count of unique values: \(generatedValues.count)")
+    }
+    
+}
