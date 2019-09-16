265fc74283744e38b986c9443ef6a777e89924de
B.T. Franklin <brandon.franklin@gmail.com>
2019-09-15 23:58:50 -0700

    Added a default initializer to CommonPersonName which provides a randomly-selected gender for the name.

--- Sources/Wordsmith/Person Names/CommonPersonName.swift
+++ Sources/Wordsmith/Person Names/CommonPersonName.swift
@@ -7,6 +7,10 @@ public struct CommonPersonName: PersonName {
     
     public private(set) var givenName: String
     public private(set) var surname: String
+    
+    public init() {
+        self.init(gender: Bool.random() ? .male : .female)
+    }
 
     public init(gender: Gender) {
         givenName = CommonPersonGivenName(gender: gender).description
