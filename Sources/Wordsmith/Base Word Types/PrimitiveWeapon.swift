3ebdebd83ebf74d04caf36154d730879e0dd08fa
B.T. Franklin <brandon.franklin@gmail.com>
2019-09-15 23:56:45 -0700

    Added the new PrimitiveWeapon and UCBerkeleyEmotion generators.

---
+++ Sources/Wordsmith/Base Word Types/PrimitiveWeapon.swift
@@ -0,0 +1,33 @@
+//  Created by B.T. Franklin on 9/11/19
+
+public struct PrimitiveWeapon {
+    
+    static let options: [String] = [
+        "sword",
+        "blade",
+        "mace",
+        "hammer",
+        "knife",
+        "dagger",
+        "axe",
+        "halberd",
+        "glaive",
+        "spear",
+        "lance",
+        "pike",
+        "bow",
+    ]
+    
+    private let value: String
+    
+    public init() {
+        value = PrimitiveWeapon.options.randomElement()!
+    }
+    
+}
+
+extension PrimitiveWeapon: CustomStringConvertible {
+    public var description: String {
+        return value
+    }
+}
