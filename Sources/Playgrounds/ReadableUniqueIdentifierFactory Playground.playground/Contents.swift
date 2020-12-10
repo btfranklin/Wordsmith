c55025466d3a0e6b4a98f916ff8c7b3c5a0a9448
B.T. Franklin <brandon.franklin@gmail.com>
2020-12-10 00:09:57 -0700

    Made sure playground ranges all start at 1 instead of 0, just for consistency.

--- Sources/Playgrounds/ReadableUniqueIdentifierFactory Playground.playground/Contents.swift
+++ Sources/Playgrounds/ReadableUniqueIdentifierFactory Playground.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Wordsmith
 
-for _ in 0...20 {
+for _ in 1...20 {
     let identifier = ReadableUniqueIdentifierFactory.sharedInstance.createReadableUniqueIdentifier()
     print(identifier)
 }
