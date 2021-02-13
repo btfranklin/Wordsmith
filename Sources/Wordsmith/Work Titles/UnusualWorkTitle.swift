d95078f687e2ee74efcc142fbbcc48726b48da4e
B.T. Franklin <brandon.franklin@gmail.com>
2021-02-13 12:07:41 -0700

    Many improvements to UnusualWorkTitle to make it much more convincing.

--- Sources/Wordsmith/Work Titles/UnusualWorkTitle.swift
+++ Sources/Wordsmith/Work Titles/UnusualWorkTitle.swift
@@ -6,10 +6,7 @@ public struct UnusualWorkTitle: Hashable {
     
     private static let formatters: [() -> String] = [
         {
-            "\(Noun()) \(Adverb()) \(Verb(tense:.present))".capitalized
-        },
-        {
-            "\(Noun())-\(Verb(tense:.presentPerfect))".capitalized
+            "\(UCBerkeleyEmotion()) \(Adverb()) \(Verb(tense:.present))".capitalized
         },
         {
             let verb = Verb(tense:.presentPerfect).description.firstUppercased
@@ -19,14 +16,14 @@ public struct UnusualWorkTitle: Hashable {
             return "\(verb) \(determiner.isArticle ? determiner.description : determiner.description.firstUppercased) \(noun)"
         },
         {
-            let opener = ["When", "Where", "Why", "As"].randomElement()!
+            let opener = ["When", "Where", "Why", "While", "As", "Until", "Because"].randomElement()!
             let noun = Noun().description.firstUppercased
             let vowelNoun = noun.startsWithVowel
             let verb = Verb(tense:.present).description.firstUppercased
-            return "\(opener) \(Determiner(forVowel: vowelNoun)) \(noun) \(verb)"
+            return "\(opener) \(Article(forVowel: vowelNoun)) \(noun) \(verb)"
         },
         {
-            let opener = ["When", "Where", "Why", "While", "As", "Until", "Unless", "Because", "And", "But"].randomElement()!
+            let opener = ["When", "Where", "Why", "While", "As", "Until", "Because"].randomElement()!
             let pronoun = Pronoun()
             let verb: String
             
@@ -43,8 +40,7 @@ public struct UnusualWorkTitle: Hashable {
                 verb = Verb(tense:.past).description.firstUppercased
             }
             
-            let elipsisPrefix = Bool.random(probability: 20) ? "..." : ""
-            return "\(elipsisPrefix)\(opener) \(pronoun.description.firstUppercased) \(verb)"
+            return "\(opener) \(pronoun.description.firstUppercased) \(verb)"
         },
         {
             let noun = Noun()
@@ -63,7 +59,7 @@ public struct UnusualWorkTitle: Hashable {
                 result = "\(Determiner(forVowel: vowelNoun)) \(noun) Is \(Verb(tense:.presentPerfect))"
 
             default:
-                result = "\(Determiner(forVowel: vowelNoun)) \(noun) Has \(Verb(tense:.presentPerfect))"
+                result = "\(Determiner(forVowel: vowelNoun)) \(noun) Has \(Verb(tense:.pastParticiple))"
             }
             
             return result.capitalized
@@ -79,79 +75,23 @@ public struct UnusualWorkTitle: Hashable {
             }
         },
         {
-            if Bool.random(probability: 20) {
-                return "\(Noun()). \(Noun()).".capitalized
-            } else {
-                let noun1 = Noun().description.firstUppercased
-                let noun2 = Noun().description.firstUppercased
-                if Bool.random() {
-                    let vowelNoun1 = noun1.startsWithVowel
-                    let vowelNoun2 = noun2.startsWithVowel
-                    let determiner1 = Determiner(forVowel: vowelNoun1).description.firstUppercased
-                    let determiner2obj = Determiner(forVowel: vowelNoun2)
-                    var determiner2 = determiner2obj.description
-                    
-                    if !determiner2obj.isArticle {
-                        determiner2 = determiner2.firstUppercased
-                    }
+            let noun1 = Noun().description.firstUppercased
+            let noun2 = Noun().description.firstUppercased
+            if Bool.random() {
+                let vowelNoun1 = noun1.startsWithVowel
+                let vowelNoun2 = noun2.startsWithVowel
+                let determiner1 = Determiner(forVowel: vowelNoun1).description.firstUppercased
+                let determiner2obj = Determiner(forVowel: vowelNoun2)
+                var determiner2 = determiner2obj.description
 
-                    return "\(determiner1) \(noun1) and \(determiner2) \(noun2)"
-                } else {
-                    return "\(noun1) and \(noun2)"
+                if !determiner2obj.isArticle {
+                    determiner2 = determiner2.firstUppercased
                 }
-            }
-        },
-        {
-            let wordCount = Int.random(in: 2...4)
-            var words: [String] = []
-            
-            switch Int.random(in: 0...2) {
-            case 0:
-                for _ in 1...wordCount {
-                    words.append(Noun().description.firstUppercased)
-                }
-            case 1:
-                for _ in 1...wordCount {
-                    words.append(Adverb().description.firstUppercased)
-                }
-            default:
-                for _ in 1...wordCount {
-                    words.append(Verb().description.firstUppercased)
-                }
-            }
-            
-            let separator: String
-            var ender: String?
-            switch Int.random(in: 0...5) {
-            case 0:
-                separator = " - "
-            case 1:
-                separator = ", "
-            case 2:
-                separator = "/"
-            case 3:
-                separator = "! "
-                ender = "!"
-            case 4:
-                separator = "; "
-            default:
-                separator = "..."
-            }
-            
-            let joinedWords = words.joined(separator: separator)
 
-            return "\(joinedWords)\(ender ?? "")"
-        },
-        {
-            let personName = CommonPersonGivenName().description
-            let possessivePersonName: String
-            if personName.last == "s" {
-                possessivePersonName = "\(personName)'"
+                return "\(determiner1) \(noun1) and \(determiner2) \(noun2)"
             } else {
-                possessivePersonName = "\(personName)'s"
+                return "\(noun1) and \(noun2)"
             }
-            
-            return "\(possessivePersonName) \(Adjective()) \(Noun())".capitalized
         },
         {
             let emotion1 = UCBerkeleyEmotion().description.firstUppercased
@@ -176,6 +116,10 @@ public struct UnusualWorkTitle: Hashable {
             
             return "\(emotionPhrase) in \(TownName())"
         },
+        {
+            let redux = ["Revisited", "Revised", "Reimagined", "Renewed", "Rethought", "Redux"].randomElement()!
+            return "'\(SimpleWorkTitle())' \(redux)"
+        }
     ]
     
     private let value: String
@@ -183,7 +127,6 @@ public struct UnusualWorkTitle: Hashable {
     public init() {
         value = UnusualWorkTitle.formatters.randomElement()!()
     }
-    
 }
 
 extension UnusualWorkTitle: CustomStringConvertible {
