088e3f74b9d11ed07139d65cfd0838bdc61d7187
B.T. Franklin <brandon.franklin@gmail.com>
2021-05-19 18:26:04 -0700

    Raised OS requirement to macOS v11. iOS requirement is still the same.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Wordsmith",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13),
+        .macOS(.v11), .iOS(.v13),
     ],
     products: [
         .library(
