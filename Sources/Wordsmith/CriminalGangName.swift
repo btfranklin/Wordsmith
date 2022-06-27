f10ab8cd9fa12625acc756c64858bd2778ab2869
B.T. Franklin <brandon.franklin@gmail.com>
2022-06-26 23:36:31 -0700

    Improved the implementation of CriminalGangName to make better use of the DSL.

--- Sources/Wordsmith/CriminalGangName.swift
+++ Sources/Wordsmith/CriminalGangName.swift
@@ -7,71 +7,60 @@ public struct CriminalGangName: TextComponent {
     public init() {}
 
     public func makeText(using randomNumberGenerator: inout some RandomNumberGenerator) -> String {
-        var generator: TextComponent
+        var component: TextComponent
 
         let beginsWithPersonName = Bool.random(probability: 0.25, using: &randomNumberGenerator)
         if beginsWithPersonName {
 
-            if Bool.random(using: &randomNumberGenerator) {
-                generator = Text(separator: " ") {
-                    GivenName().possessiveForm()
+            component = Text(separator: " ") {
+                GivenName().possessiveForm()
+                OneOf {
                     VillainousPersonNoun(isPlural: true)
-                }.titleCased()
-            } else {
-                generator = Text(separator: " ") {
-                    GivenName().possessiveForm()
                     PrimitiveWeapon(isPlural: true)
-                }.titleCased()
-            }
+                }
+            }.titleCased()
 
         } else {
-            switch Int.random(in: 1...6, using: &randomNumberGenerator) {
-            case 1:
-                generator = Text(separator: " ") {
-                    MartialSocialConcept()
-                    VillainousPersonNoun(isPlural: true)
-                }
 
-            case 2:
-                generator = Text(separator: " ") {
-                    PrimitiveWeapon()
-                    VillainousPersonNoun(isPlural: true)
-                }
-
-            case 3:
-                generator = Text(separator: " ") {
-                    VillainousPersonNoun(isPlural: true)
-                    "of"
-                    TownName()
-                }
-
-            case 4:
-                generator = Text(separator: " ") {
-                    TownName()
-                    VillainousPersonNoun(isPlural: true)
-                }
-
-            case 5:
-                generator = Text(separator: " ") {
-                    Adjective()
-                    VillainousPersonNoun(isPlural: true)
-                }
-
-            default:
-                generator = Text(separator: " ") {
-                    Adjective()
-                    VillainousPersonNoun(isPlural: true)
-                    "of"
-                    TownName()
-                }
-            }
-
-            generator = Text(separator: " ") {
+            component = Text(separator: " ") {
                 "the"
-                generator.titleCased()
+                OneOf {
+                    Text(separator: " ") {
+                        MartialSocialConcept()
+                        VillainousPersonNoun(isPlural: true)
+                    }
+
+                    Text(separator: " ") {
+                        PrimitiveWeapon()
+                        VillainousPersonNoun(isPlural: true)
+                    }
+
+                    Text(separator: " ") {
+                        VillainousPersonNoun(isPlural: true)
+                        "of"
+                        TownName()
+                    }
+
+                    Text(separator: " ") {
+                        TownName()
+                        VillainousPersonNoun(isPlural: true)
+                    }
+
+                    Text(separator: " ") {
+                        Adjective()
+                        VillainousPersonNoun(isPlural: true)
+                    }
+
+                    Text(separator: " ") {
+                        Adjective()
+                        VillainousPersonNoun(isPlural: true)
+                        "of"
+                        TownName()
+                    }
+                }.titleCased()
             }
         }
 
-        return generator.makeText(using: &randomNumberGenerator)
+        return component.makeText(using: &randomNumberGenerator)
     }
 }
