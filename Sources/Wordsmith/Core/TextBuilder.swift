9095155167a2697031db85a77b1f0d915cc036ca
B.T. Franklin <brandon.franklin@gmail.com>
2022-07-05 20:46:05 -0700

    Added "public" access to the TextBuilder

--- Sources/Wordsmith/Core/TextBuilder.swift
+++ Sources/Wordsmith/Core/TextBuilder.swift
@@ -1,35 +1,35 @@
 //  Created by B.T. Franklin on 6/10/22
 
 @resultBuilder
-enum TextBuilder {
+public enum TextBuilder {
 
-    static func buildBlock() -> [TextComponent] {
+    public static func buildBlock() -> [TextComponent] {
         [EmptyText() as TextComponent]
     }
 
-    static func buildBlock(_ components: [TextComponent]...) -> [TextComponent] {
+    public static func buildBlock(_ components: [TextComponent]...) -> [TextComponent] {
         components.reduce([], { partialResult, element in
             partialResult + element
         })
     }
 
-    static func buildBlock(_ components: TextComponent...) -> [TextComponent] {
+    public static func buildBlock(_ components: TextComponent...) -> [TextComponent] {
         components.compactMap { $0 }
     }
 
-    static func buildBlock(_ components: String...) -> [String] {
+    public static func buildBlock(_ components: String...) -> [String] {
         components.compactMap { $0 }
     }
 
-    static func buildOptional(_ components: [TextComponent]?) -> [TextComponent] {
+    public static func buildOptional(_ components: [TextComponent]?) -> [TextComponent] {
         [components] as? [TextComponent] ?? [EmptyText()]
     }
 
-    static func buildEither(first components: [TextComponent]) -> [TextComponent] {
+    public static func buildEither(first components: [TextComponent]) -> [TextComponent] {
         components
     }
 
-    static func buildEither(second components: [TextComponent]) -> [TextComponent] {
+    public static func buildEither(second components: [TextComponent]) -> [TextComponent] {
         components
     }
 
